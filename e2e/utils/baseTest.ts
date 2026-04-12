20519563a81c23c69e2dddb32d0966ab11747f19
lakshmirqaoncloud <lakshmi@qaoncloud.com>
2026-04-12 17:29:15 +0530

    sorting

--- e2e/utils/baseTest.ts
+++ e2e/utils/baseTest.ts
@@ -5,38 +5,40 @@ import { CartPage } from '../pages/CartPage';
 import { CheckOutPage } from '../pages/CheckOutPage';
 import { TestData } from './testData';
 
-type Pages = {
+type Fixtures = {
   loginPage: LoginPage;
   inventoryPage: InventoryPage;
   cartPage: CartPage;
   checkOutPage: CheckOutPage;
-  authenticatedPage: Page; // page after login
+  authenticatedPage: Page;
 };
 
-// Extend base test with custom fixtures
-export const test = base.extend<Pages>({
-  loginPage: async ({ page }: { page: Page }, use) => {
+export const test = base.extend<Fixtures>({
+  loginPage: async ({ page }, use) => {
     await use(new LoginPage(page));
   },
 
-  authenticatedPage: async ({ page, loginPage }: { page: Page; loginPage: LoginPage }, use) => {
-    const user = TestData.getUser('STANDARD'); // get credentials dynamically
-    await loginPage.navigateToLoginPage();
+  authenticatedPage: async ({ page }, use) => {
+    const loginPage = new LoginPage(page);
+    const user = TestData.getUser('STANDARD');
+
+    await loginPage.goto('/');
     await loginPage.login(user.username, user.password);
-    await use(page); // page is now authenticated
+
+    await use(page);
   },
 
-  inventoryPage: async ({ authenticatedPage }: { authenticatedPage: Page }, use) => {
+  inventoryPage: async ({ authenticatedPage }, use) => {
     await use(new InventoryPage(authenticatedPage));
   },
 
-  cartPage: async ({ authenticatedPage }: { authenticatedPage: Page }, use) => {
+  cartPage: async ({ authenticatedPage }, use) => {
     await use(new CartPage(authenticatedPage));
   },
 
-  checkOutPage: async ({ authenticatedPage }: { authenticatedPage: Page }, use) => {
+  checkOutPage: async ({ authenticatedPage }, use) => {
     await use(new CheckOutPage(authenticatedPage));
   },
 });
 
-export const expect = base.expect; // optional, can also import directly from @playwright/test
+export { expect } from '@playwright/test';
---
+++ e2e/utils/constants.ts
@@ -0,0 +1,11 @@
+export const SortOptions = {
+  NAME_ASC: 'az',
+  NAME_DESC: 'za',
+  PRICE_LOW_HIGH: 'lohi',
+  PRICE_HIGH_LOW: 'hilo'
+};
+export const Routes = {
+  BASE_URL: 'https://www.saucedemo.com',
+  LOGIN: '/',
+  INVENTORY: '/inventory.html'
+};
